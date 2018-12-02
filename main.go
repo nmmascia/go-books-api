bc48dabe055bd908e54edb78b2e30fd469f07405
Nicholas Mascia <nmmascia@gmail.com>
2018-12-02 14:27:01 -0500

    Add basic structure

--- main.go
+++ main.go
@@ -1,5 +1,54 @@
 package main
 
-func main() {
+import (
+	// "encoding/json"
+	"log"
+	"net/http"
 
+	// "math/rand"
+	// "strconv"
+	"github.com/gorilla/mux"
+)
+
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func createBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/api/books", getBooks).Methods("GET")
+	router.HandleFunc("/api/books", createBook).Methods("POST")
+	router.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	router.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	router.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
