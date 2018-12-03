a7c07e64615726671cfe992816f9a21d82dad8b9
Nicholas Mascia <nmmascia@gmail.com>
2018-12-02 19:13:37 -0500

    Remove code specific to in memory store

--- main.go
+++ main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
 	"log"
-	"math/rand"
 	"net/http"
 	"os"
-	"strconv"
 
 	"github.com/gorilla/mux"
 	_ "github.com/joho/godotenv/autoload"
@@ -17,82 +14,48 @@ import (
 
 // Book a book struct
 type Book struct {
-	ID     string  `json:"id"`
-	Isbn   string  `json:"isbn"`
-	Title  string  `json:"title"`
-	Author *Author `json:"author"`
+	ID    string `json:"id"`
+	Isbn  string `json:"isbn"`
+	Title string `json:"title"`
 }
-
-// Author an author struct
-type Author struct {
-	Firstname string `json:"firstname"`
-	Lastname  string `json:"lastname"`
-}
-
-var books []Book
 
 func getBooksHandler(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(books)
+		// json.NewEncoder(w).Encode(books)
 	}
 }
 
 func createBookHandler(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		var book Book
-		_ = json.NewDecoder(r.Body).Decode(&book)
-		book.ID = strconv.Itoa(rand.Intn(1000000))
-		books = append(books, book)
-		json.NewEncoder(w).Encode(book)
+		// var book Book
+		// _ = json.NewDecoder(r.Body).Decode(&book)
+		// json.NewEncoder(w).Encode()
 	}
 }
 
 func getBookHandler(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		params := mux.Vars(r)
-		for _, item := range books {
-			if item.ID == params["id"] {
-				json.NewEncoder(w).Encode(item)
-				return
-			}
-		}
-
-		json.NewEncoder(w).Encode(&Book{})
+		// params := mux.Vars(r)
+		// json.NewEncoder(w).Encode()
 	}
 }
 
 func updateBookHandler(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		var book Book
-		params := mux.Vars(r)
-		for index, item := range books {
-			if item.ID == params["id"] {
-				books = append(books[:index], books[index+1:]...)
-				_ = json.NewDecoder(r.Body).Decode(&book)
-				book.ID = item.ID
-				books = append(books, book)
-				json.NewEncoder(w).Encode(book)
-				break
-			}
-		}
+		// params := mux.Vars(r)
+		// json.NewEncoder(w).Encode()
 	}
 }
 
 func deleteBookHandler(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		params := mux.Vars(r)
-		for index, item := range books {
-			if item.ID == params["id"] {
-				books = append(books[:index], books[index+1:]...)
-				break
-			}
-		}
-		json.NewEncoder(w).Encode(books)
+		// params := mux.Vars(r)
+		// json.NewEncoder(w).Encode()
 	}
 }
 
