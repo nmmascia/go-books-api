dd2dd83a25279ef5ab373c780ebdb7dd00bac0b8
Nicholas Mascia <nmmascia@gmail.com>
2018-12-02 19:08:14 -0500

    Use closures to pass in db to handler functions

--- main.go
+++ main.go
@@ -31,59 +31,69 @@ type Author struct {
 
 var books []Book
 
-func getBooks(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(books)
-}
-
-func createBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var book Book
-	_ = json.NewDecoder(r.Body).Decode(&book)
-	book.ID = strconv.Itoa(rand.Intn(1000000))
-	books = append(books, book)
-	json.NewEncoder(w).Encode(book)
-}
-
-func getBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for _, item := range books {
-		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
-			return
-		}
-	}
-
-	json.NewEncoder(w).Encode(&Book{})
-}
-
-func updateBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var book Book
-	params := mux.Vars(r)
-	for index, item := range books {
-		if item.ID == params["id"] {
-			books = append(books[:index], books[index+1:]...)
-			_ = json.NewDecoder(r.Body).Decode(&book)
-			book.ID = item.ID
-			books = append(books, book)
-			json.NewEncoder(w).Encode(book)
-			break
-		}
+func getBooksHandler(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(books)
 	}
 }
 
-func deleteBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for index, item := range books {
-		if item.ID == params["id"] {
-			books = append(books[:index], books[index+1:]...)
-			break
+func createBookHandler(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		var book Book
+		_ = json.NewDecoder(r.Body).Decode(&book)
+		book.ID = strconv.Itoa(rand.Intn(1000000))
+		books = append(books, book)
+		json.NewEncoder(w).Encode(book)
+	}
+}
+
+func getBookHandler(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		params := mux.Vars(r)
+		for _, item := range books {
+			if item.ID == params["id"] {
+				json.NewEncoder(w).Encode(item)
+				return
+			}
+		}
+
+		json.NewEncoder(w).Encode(&Book{})
+	}
+}
+
+func updateBookHandler(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		var book Book
+		params := mux.Vars(r)
+		for index, item := range books {
+			if item.ID == params["id"] {
+				books = append(books[:index], books[index+1:]...)
+				_ = json.NewDecoder(r.Body).Decode(&book)
+				book.ID = item.ID
+				books = append(books, book)
+				json.NewEncoder(w).Encode(book)
+				break
+			}
 		}
 	}
-	json.NewEncoder(w).Encode(books)
+}
+
+func deleteBookHandler(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		params := mux.Vars(r)
+		for index, item := range books {
+			if item.ID == params["id"] {
+				books = append(books[:index], books[index+1:]...)
+				break
+			}
+		}
+		json.NewEncoder(w).Encode(books)
+	}
 }
 
 func createDB() *sql.DB {
@@ -99,17 +109,13 @@ func createDB() *sql.DB {
 
 func main() {
 	db := createDB()
-
 	router := mux.NewRouter()
 
-	books = append(books, Book{ID: "1", Isbn: "448743", Title: "Book One", Author: &Author{Firstname: "John", Lastname: "Doe"}})
-	books = append(books, Book{ID: "2", Isbn: "448744", Title: "Book Two", Author: &Author{Firstname: "Jane", Lastname: "Doe"}})
-
-	router.HandleFunc("/api/books", getBooks).Methods("GET")
-	router.HandleFunc("/api/books", createBook).Methods("POST")
-	router.HandleFunc("/api/books/{id}", getBook).Methods("GET")
-	router.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
-	router.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+	router.HandleFunc("/api/books", getBooksHandler(db)).Methods("GET")
+	router.HandleFunc("/api/books", createBookHandler(db)).Methods("POST")
+	router.HandleFunc("/api/books/{id}", getBookHandler(db)).Methods("GET")
+	router.HandleFunc("/api/books/{id}", updateBookHandler(db)).Methods("PUT")
+	router.HandleFunc("/api/books/{id}", deleteBookHandler(db)).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
